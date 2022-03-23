d0c63c81f6a3a5bb9b4fa05413c18fd9865afa03
Ignacio Pieve <ignacio.pieve@gmail.com>
2022-03-23 18:21:33 -0300

    Init

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+
+# Importacion de modulos
+from api.test import test
+
+app = Flask(__name__, template_folder='./webpage/build',
+            static_url_path='',
+            static_folder='./webpage/build')
+CORS(app)
+
+# Registramos los modulos
+app.register_blueprint(test, url_prefix='/api/test')
+
+if __name__ == '__main__':
+    app.run(debug=True)
