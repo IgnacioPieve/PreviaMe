6f30ec783604286816894a6e094d891767683980
Ignacio Pieve <ignacio.pieve@gmail.com>
2022-04-09 21:20:48 -0300

    Starting to prepare to implement Docker

---
+++ config_example.py
@@ -0,0 +1,35 @@
+title = 'PreviaMe'
+version = 0.1
+metadata = {
+    "title": title,
+    "version": version,
+    "contact": {
+        "name": "",
+        "email": "",
+    },
+    "description": ""
+}
+
+db_string = ''
+db_name = ''
+
+credentials = {
+    "firebase_key": "",
+    "firebase": {
+        "type": "",
+        "project_id": "",
+        "private_key_id": "",
+        "private_key": "",
+        "client_email": "",
+        "client_id": "",
+        "auth_uri": "",
+        "token_uri": "",
+        "auth_provider_x509_cert_url": "",
+        "client_x509_cert_url": ""
+    },
+    "server": {
+        "private_key": "",
+        "host": "",
+        "username": ""
+    }
+}
