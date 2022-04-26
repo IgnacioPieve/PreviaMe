73410c6e1acfad86f3a1584881e95a459cd5e7e4
Ignacio Pieve <ignacio.pieve@gmail.com>
2022-04-25 22:55:29 -0300

    :test_tube: Testing black

--- src/app.py
+++ src/app.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+
+
 from routers import user, friend, party, test
+
+
+
+
 import config
 
 app = FastAPI(**config.metadata)
@@ -8,3 +14,9 @@ app.include_router(user.router)
 app.include_router(friend.router)
 app.include_router(party.router)
 app.include_router(test.router)
+
+
+
+
+
+
