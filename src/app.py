c038950479666e46444e22691d4f688dec572134
[Bot] Ignacio Pieve Roiger <ignacio.pieve@gmail.com>
2022-04-27 21:14:07 +0000

    🎨 [Bot] Format Python code with Black

--- src/app.py
+++ src/app.py
@@ -20,11 +20,30 @@ app.include_router(test.router)
 async def log_request(request: Request, call_next):
     try:
         response = await call_next(request)
-        await db['log'].insert_one({'success': True, 'date': datetime.datetime.now(), 'url': str(request.url), 'method': request.method, 'headers': dict(request.headers)})
+        await db["log"].insert_one(
+            {
+                "success": True,
+                "date": datetime.datetime.now(),
+                "url": str(request.url),
+                "method": request.method,
+                "headers": dict(request.headers),
+            }
+        )
         return response
 
     except Exception as e:
-        await db['log'].insert_one({'success': False, 'date': datetime.datetime.now(), 'url': str(request.url), 'method': request.method, 'headers': dict(request.headers), 'error': str(traceback.format_exc())})
+        await db["log"].insert_one(
+            {
+                "success": False,
+                "date": datetime.datetime.now(),
+                "url": str(request.url),
+                "method": request.method,
+                "headers": dict(request.headers),
+                "error": str(traceback.format_exc()),
+            }
+        )
         raise e
-db['log'].create_index([('date', 1)])
+
+
+db["log"].create_index([("date", 1)])
 app.middleware("http")(log_request)
