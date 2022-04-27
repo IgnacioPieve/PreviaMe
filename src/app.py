a529eec5d1b1ef73d4605a74c73a096d259aceb7
Ignacio Pieve <ignacio.pieve@gmail.com>
2022-04-27 13:30:26 -0300

    Added logs

--- src/app.py
+++ src/app.py
@@ -1,11 +1,17 @@
+import logging
+
 from fastapi import FastAPI
 
 import config
-from routers import friend, party, test, user
+from routers import friend, manage, party, test, user
 
+logging.basicConfig(filename='.log', encoding='utf-8', level=logging.INFO)
 app = FastAPI(**config.metadata)
 
 app.include_router(user.router)
 app.include_router(friend.router)
 app.include_router(party.router)
+
+app.include_router(manage.router)
 app.include_router(test.router)
+
---
+++ src/routers/manage.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
+
+router = APIRouter(prefix="/manage", tags=["Manage"])
+
+
+# endpoint to see logs
+@router.get("/logs", response_class=PlainTextResponse)
+async def get_logs():
+    with open(".log", encoding='utf-8') as f:
+        return f.read()
