79c68a6b8051a0287066ffb6b1f5ed7dc0bb913b
Ignacio Pieve <ignacio.pieve@gmail.com>
2022-04-26 23:30:45 -0300

    I really hope this fixes the deployment

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
-import os
 import json
+import os
 
 title = "PreviaMe"
 version = 0.1
@@ -17,10 +17,27 @@ metadata = {
     "description": "¿Qué esperas para organizar tus previas?",  # Aca se puede poner markdown
 }
 
+
+def env_variable_to_dict(env_variable):
+    try:
+        return json.loads(env_variable)
+    except json.JSONDecodeError:
+        env_variable = env_variable.split(',')
+        env_variable[0] = env_variable[0].replace('{', '')
+        env_variable[-1] = env_variable[-1].replace('}', '')
+        new_dict = {}
+
+        for key_value in env_variable:
+            key_value = key_value.split(':')
+            new_dict[key_value[0].strip()] = key_value[1].strip()
+
+        return new_dict
+
+
 db_string = os.getenv("PREVIAME_DB_STRING")
 db_name = "PreviaMe"
 
-firebase_json = json.loads(os.getenv("PREVIAME_FIREBASE_JSON"))
+firebase_json = env_variable_to_dict(os.getenv("PREVIAME_FIREBASE_JSON"))
 firebase_private_key = os.getenv("PREVIAME_FIREBASE_PRIVATE_KEY").replace("\\n", "\n")
 firebase = {**firebase_json, "private_key": firebase_private_key}
 firebase_key = os.getenv("PREVIAME_FIREBASE_KEY")
